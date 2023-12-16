4d171aca2c5ae18772d8f8adf5ba3f6a30b55397
Karsten Kirby Uy <karsten.uy@gmail.com>
2023-12-15 20:51:24 -0800

    Commected Low Cut parameters to filter

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,10 +10,18 @@
 
 #include <JuceHeader.h>
 
+enum Slope // enums can be expressed as integers
+{
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
 struct ChainSettings {
     float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.f };
     float lowCutFreq{ 0 }, highCutFreq{ 0 };
-    int lowCutSlope{ 0 }, highCutSlope{ 0 };
+    int lowCutSlope{ Slope::Slope_12 }, highCutSlope{ Slope::Slope_12 };
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
