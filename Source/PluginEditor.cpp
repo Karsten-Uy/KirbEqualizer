70d114fd2b1f7245fe3e723a6c02e2654a59dd74
Karsten Kirby Uy <karsten.uy@gmail.com>
2023-12-19 13:29:14 -0800

    Respone curves working

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -457,6 +457,17 @@ void SimpleEQAudioProcessorEditor::timerCallback()
         auto peakCoefficients = makePeakFilter(chainSettings,audioProcessor.getSampleRate());
         updateCoefficients(monoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
         
+        auto lowCutCoefficients = makeLowCutFilter(chainSettings,audioProcessor.getSampleRate());
+        auto highCutCoefficients = makeHighCutFilter(chainSettings, audioProcessor.getSampleRate());
+
+        updateCutFilter(monoChain.get<ChainPositions::LowCut>(),
+            lowCutCoefficients,
+            static_cast<Slope>(chainSettings.lowCutSlope));
+
+        updateCutFilter(monoChain.get<ChainPositions::HighCut>(),
+            highCutCoefficients,
+            static_cast<Slope>(chainSettings.highCutSlope));
+
         //signal repaint
         repaint();
     }
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -38,58 +38,44 @@ void updateCoefficients(Coefficients& old, const Coefficients& replacements);
 Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
 
 
-template<typename ChainType, typename CoefficientType>
-void updateCutFilter(
-
-    ChainType& leftLowCut,
-    const CoefficientType& cutCoefficients,
-    const Slope& lowCutSlope)
+template<int Index, typename ChainType, typename CoefficientType>
+void update(ChainType& chain, const CoefficientType& coefficients)
 {
-    leftLowCut.template setBypassed<0>(true); // initially sets all filters in a cutFilter to not be on
-    leftLowCut.template setBypassed<1>(true);
-    leftLowCut.template setBypassed<2>(true);
-    leftLowCut.template setBypassed<3>(true);
+    updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+    chain.template setBypassed<Index>(false);
+}
 
-    switch (lowCutSlope)
+template<typename ChainType, typename CoefficientType>
+void updateCutFilter(ChainType& chain,
+    const CoefficientType& coefficients,
+    const Slope& slope)
+{
+    chain.template setBypassed<0>(true);
+    chain.template setBypassed<1>(true);
+    chain.template setBypassed<2>(true);
+    chain.template setBypassed<3>(true);
+
+    switch (slope)
     {
-        case Slope_12:
-        {
-            *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<0>(false);
-            break;
-        }
-        case Slope_24:
-        {
-            *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<0>(false);
-            *leftLowCut.template get<1>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<1>(false);
-            break;
-        }
-        case Slope_36:
-        {
-            *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<0>(false);
-            *leftLowCut.template get<1>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<1>(false);
-            *leftLowCut.template get<2>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<2>(false);
-            break;
-        }
-        case Slope_48:
-        {
-            *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<0>(false);
-            *leftLowCut.template get<1>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<1>(false);
-            *leftLowCut.template get<2>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<2>(false);
-            *leftLowCut.template get<3>().coefficients = *cutCoefficients[0];
-            leftLowCut.template setBypassed<3>(false);
-            break;
-        }
+    case Slope_48:
+    {
+        update<3>(chain, coefficients);
+    }
+    case Slope_36:
+    {
+        update<2>(chain, coefficients);
+    }
+    case Slope_24:
+    {
+        update<1>(chain, coefficients);
+    }
+    case Slope_12:
+    {
+        update<0>(chain, coefficients);
+    }
     }
 }
+
 
 inline auto makeLowCutFilter(const ChainSettings& chainSettings, double sampleRate)
 {
